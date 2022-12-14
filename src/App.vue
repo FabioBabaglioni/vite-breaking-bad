903938b87e7c4d663fcf3170c34a77f53531ab1d
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-14 17:39:58 +0100

    Importo oggetto store

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import { store } from `./store`
 
 
 export default {
   components: {
     AppHeader, AppMain,
+  },
+  data() {
+    return {
+      store,
+    }
   }
 }
+
 </script>
 
 <template>
