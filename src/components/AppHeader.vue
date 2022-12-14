8425fa5fbd9cf8fed2b32fa32bbfa6911d3da0b0
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-14 16:53:16 +0100

    Creo componente appHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,9 +6,21 @@ export default {
 </script>
 
 <template>
-    <h1>Ciao</h1>
+    <section class="d-flex">
+        <img src="https://vectorlogo4u.com/wp-content/uploads/2020/11/Rick-and-Morty-Logo-Vector-01.png" alt="">
+        <h1 class="pt-2 px-1">Rick and Morty Api</h1>
+    </section>
+
 </template>
 
 <style scoped lang="scss">
+@use "./style/partials/variables.scss" as *;
 
+h1 {
+    color: $brandSecondary;
+}
+
+img {
+    width: 150px;
+}
 </style>
