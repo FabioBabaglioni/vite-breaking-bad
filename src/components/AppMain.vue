4c5d197ab931b912ec0103e58d7493fa88a40875
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-14 16:55:43 +0100

    Creo componente AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+
+}
+</script>
+
+<template>
+    <section class="">
+
+    </section>
+
+</template>
+
+<style scoped lang="scss">
+@use "./style/partials/variables.scss" as *;
+</style>
